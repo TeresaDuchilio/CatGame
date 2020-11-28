d1095a582d742ff68a1eff69b25b8ee42577cfe2
Teresa <teresaduchilio@gmail.com>
2020-11-28 18:28:44 +0100

    Some more objects

--- Assets/Scripts/GameManagement/GameFlowManager.cs
+++ Assets/Scripts/GameManagement/GameFlowManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class GameFlowManager : MonoBehaviour
@@ -6,6 +7,7 @@ public class GameFlowManager : MonoBehaviour
     public GameObject GeorgeFlurPicture;
 
     GameObject FredKitchen;
+    InteractObject Boxes;
     EventManager eventManager;
     GameState gameState;
     Cutscene cutscene;
@@ -39,6 +41,9 @@ public class GameFlowManager : MonoBehaviour
             case 3:
                 //animation
                 FredKitchen = GameObject.FindGameObjectWithTag("FredKitchen");
+                Boxes = GameObject.FindGameObjectWithTag("Boxes").GetComponent<InteractObject>();
+                Boxes.hasItem = true;
+                Boxes.inspectText = new List<string>() { "Leckerlies!", "Ich hab viel zu kurze Arme" };
                 cutscene.PlayWalkCutscene(FredKitchen, new Vector3(-6, 0.5f, -5.5f), new Vector3(-5.5f, 0.5f, -0.5f));
                 gameState.gameFlowId = 3;
                 //activate leckerlie box
